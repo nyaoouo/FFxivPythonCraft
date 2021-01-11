5e3b03a6bae7f32839365249578deb1479c081b4
nyaoouo <kikiandchch@gmail.com>
2021-01-11 23:25:43 +0800

    图纸使用增加lv判断

--- solver_v2/stages/Stage4.py
+++ solver_v2/stages/Stage4.py
@@ -4,12 +4,24 @@ from . import StageBase
 combo = ['阔步', '改革', '观察', '注视加工', '阔步', '比尔格的祝福', '制作']
 
 
+def score_grade(score: int):
+    if score < 5800:
+        return 0
+    elif score < 6500:
+        return 1
+    elif score < 7700:
+        return 2
+    else:
+        return 3
+
+
 class Stage4(StageBase):
     def __init__(self, solver):
         super().__init__(solver)
         self.Prequeue = []
         self.count = 0
         self.use_change = self.solver.conf.get('DesignChange', 'open') == 'True'
+        self.change_grade=int(self.solver.conf.get('DesignChange', 'lv',default="3"))
 
     def deal(self, status, prev_skill=None):
         if prev_skill.name == '设计变动':
@@ -19,7 +31,7 @@ class Stage4(StageBase):
             return '设计变动'
         self.solver.cli_logger.showTag('Math')
         self.count += 1
-        if self.count >= len(combo) and status.currentQuality < 58000:
+        if self.count >= len(combo) and score_grade(status.currentQuality) < 1:
             return 'terminate'
         return self.Prequeue.pop(0)
 
@@ -36,12 +48,14 @@ class Stage4(StageBase):
         if status.ball == BallManager.RedBall:
             self.log("red ball")
             return False
-        current_final = status.use_skill(SkillManager['比尔格的祝福']).currentQuality
-        if current_final > 77000:
-            self.log('no need',current_final)
+        current_score=status.use_skill(SkillManager['比尔格的祝福']).currentQuality
+        current_grade = score_grade(current_score)
+        if current_grade >= 3:
+            self.log('no need', current_score)
             return False
         temp = status.clone()
         temp.ball = BallManager.RedBall
-        mark = temp.use_skill(SkillManager['比尔格的祝福']).currentQuality
-        self.log(mark)
-        return mark > 77000
+        new_score=temp.use_skill(SkillManager['比尔格的祝福']).currentQuality
+        new_grade =  score_grade(new_score)
+        self.log("score if use:",new_score)
+        return new_grade >= self.change_grade and new_grade>current_grade
